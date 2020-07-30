bf68c16f50d424aa2377e3bad290b6e66cc223be
Mathew Murray <the.thran@gmail.com>
2020-07-30 16:02:11 -0400

    Update AddNote.js

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -5,7 +5,7 @@ import ApiContext from '../ApiContext';
 import config from '../config';
 import CircleButton from '../CircleButton/CircleButton';
 
-export default class AddNote extends React.Component{
+class AddNote extends React.Component{
     static contextType = ApiContext;
     constructor(props){
         super(props)
@@ -33,7 +33,16 @@ export default class AddNote extends React.Component{
 
     validateName() {
         const {value,touched} = this.state.name;
+        return ( 
+            typeof value === 'string' &&
+            value.length > 0 &&
+            touched
+        );
     }
+    validateFolder = () => {
+        return this.state.folder.value;
+    }
+
     handleSubmit = (e) => {
         e.preventDefault()
         const {name, content, folder} = this.state;
@@ -60,16 +69,61 @@ export default class AddNote extends React.Component{
             this.props.history.push('/');
         });
     }
+
+    renderAlt = () => {
+        return (
+            <>
+                <h2>Please create a folder first</h2>
+                <CircleButton
+                    tag={Link}
+                    to='/add-folder'
+                    type='button'
+                    className='NoteListNav__add-folder-button'
+                >
+                    <FontAwesomeIcon icon='plus' />
+                    <br />
+                    folder
+                </CircleButton>
+            </>
+        );
+    };
+    renderForm = () => {
+        return (
+            <form onSubmit={(e) => this.handleSubmit(e)}>
+                <h2>Add New Note</h2>
+                <label htmlFor='name'>Name:</label>
+                <input
+                    type='text'
+                    name='name'
+                    id='name'
+                    onChange={(e) = > this.updateName(e.target.value)}
+                />
+                <textarea onChange={(e) = > this.upDateContent(e.target.value)}
+                    id='content'
+                    aria-label='note content'
+                    defaultValue='Note Content'
+                />
+                <label htmlFor='folder'>Folder:</label>
+                <select name='folder' onChange={(e) => this.updateFolder(e.target.value)}>
+                    <option value=''>Choose</option>
+                    {this.renderFolderSelection()}
+                </select>
+                <button
+                disabled={!this.validateName() || ! this.validateFolder()}
+                type='submit'
+                className='submit-new-folder'
+                >Add</button>
+            </form>
+        );
+    };
+
     render(){
         return(
-            <div>
-                <form>
-                    <input id='new_note' type='text' value={this.state.value}
-                    onChange={e => this.setState(e.target.value)}>
-                    </input>
-                </form>
-                <button>submit</button>
-            </div>
-        )
+           <>
+            {this.context.folders.length > 0 && this.renderForm()}
+            {this.context.folders.length <= 0 && this.renderAlt()}
+        </>
+        );
     }
 }
+export default withRouter(AddNote);
