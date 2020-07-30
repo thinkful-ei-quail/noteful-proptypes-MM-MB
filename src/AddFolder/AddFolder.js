c9500391ec937e7a547c22f6bf053f81c2e87a9c
Mathew Murray <the.thran@gmail.com>
2020-07-30 12:21:29 -0400

    basic layout of components

---
+++ src/AddFolder/AddFolder.js
@@ -0,0 +1,22 @@
+/*Create a new component AddFolder that implements a form to capture the name
+ of a new folder from the user. This form should submit the name of the new 
+ folder to the POST /folders endpoint on the server. Ensure that any errors 
+ are properly handled. Add a button to the navigation to invoke the new form. */
+
+
+import React from 'react';
+
+export default class AddFolder extends React.Component{
+    render(){
+        return(
+            <div>
+                <form>
+                    <input type = 'text'>
+
+                    </input>
+                </form>
+                <button>Submit</button>
+            </div>
+        )
+    }
+}
---
+++ src/AddNote/AddNote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class AddNote extends React.Component{
+    render(){
+        return(
+            <div>
+                <form>
+                    <input type='text'>
+                    </input>
+                </form>
+                <button>submit</button>
+            </div>
+        )
+    }
+}
