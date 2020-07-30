df432e744aca2ba28fba19f94aef1933afe82e99
Mathew Murray <the.thran@gmail.com>
2020-07-30 15:45:58 -0400

    note functionality

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,36 +1,65 @@
 import React from 'react';
+import {link, withRouter } from 'react-router-dom;'
+import { FontAwesomeIcon } from '@fontawesome/react-fontawsome';
+import ApiContext from '../ApiContext';
+import config from '../config';
+import CircleButton from '../CircleButton/CircleButton';
 
 export default class AddNote extends React.Component{
+    static contextType = ApiContext;
     constructor(props){
         super(props)
         this.state = {
-            newNote: {
-                name:' ',
+                name:{value:'', touched: false},
+                content: {value:'', touched: false},
+                folder: {value: ''}
 
         }
     }
-    function handleAddFolder = e => {
-        e.preventDefault()
-        let formData = new FormData();
+    updateName = (name) =>{
+    this.setState({name:{value: name, touched: true}});
+    }
+    upDateContent = (content) => {
+        this.setState({ content: { value: content, touched: true}});
+    }
+    updateFolder = (id) => {
+        this.setState({folder: {value: id}});
+    }
+    renderFolderSelection = () => {
+        return this.context.folders.map((folder) => {
+            return <option key={folder.id} value={folder.id}>{folder.name}</option>;
+        });
+    };
 
-        let requestOptions = {
+    validateName() {
+        const {value,touched} = this.state.name;
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const {name, content, folder} = this.state;
+        let formNote = JSON.stringify(
+            {
+                name: name.value,
+                content: content.value,
+                folderId: folder.value,
+                modified: new Date(date.now())
+            }
+        );
+
+        const requestOptions = {
             method: 'POST',
-            body: formData
+            headers: {'Content-Type': 'application/json'},
+            body: formNote
         };
 
         fetch(`${config.API_ENDPOINT}/notes`, requestOptions)
-        .then(res =>{
-            if(!res.ok)
-                return res.json().then(e => Promise.reject(e))
-            return res.json()
-        })
-        .then(res => {
-            this.context.AddNote(note.id)
-        })
-        .catch(error => {
-            console.error({error})
-        })
-    };
+        .then((res) => res.json())
+        .then((res => {
+            console.log(res);
+            this.context.addNote(res);
+            this.props.history.push('/');
+        });
+    }
     render(){
         return(
             <div>
