150c94455869cf1f430cea6ca7089a547585f122
Mathew Murray <the.thran@gmail.com>
2020-09-07 00:26:06 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    API_ENDPOINT: 'http://localhost:9090'
+    API_ENDPOINT: 'postgres://ptyonzfzhnixmw:4e579f19779ad14f45f89033193264039f7c3be1562778f9199c44dfe7aed2cf@ec2-52-72-34-184.compute-1.amazonaws.com:5432/d9mgfnlj0u6l67'
   }
